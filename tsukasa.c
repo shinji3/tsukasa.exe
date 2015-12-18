057bde581f2f10ee1153ccc904a4f0f12cdd5ce4
shinji3 <renrakusaki2003@yahoo.co.jp>
2015-12-19 03:40:56 +0900

    VBR MP3のWMP11への対応

--- tsukasa.c
+++ tsukasa.c
@@ -375,10 +375,10 @@ int main(int argc, char *argv[]) {
 					average_number_of_bytes_per_second += (unsigned long)data[data_pos+86+i] << 8 * i;
 				}
 
-				/* average_number_of_bytes_per_secondが0の場合はaverage_number_of_bytes_per_secondに1を書き込む */
+				/* average_number_of_bytes_per_secondが0の場合はaverage_number_of_bytes_per_secondに0xFFFFFFFFを書き込む */
 				if (average_number_of_bytes_per_second == 0) {
 					for (i=0;i<4;i++) {
-						data[data_pos+86+i] = (1 >> 8 * i) & 0xFF;
+						data[data_pos+86+i] = (0xFFFFFFFF >> 8 * i) & 0xFF;
 					}
 				}
 			}
